7a9afd0d6c7e1a276249454c5bf98e3383b2e795
Toulix <tolojanaharypatrickandre@gmail.com>
2022-08-01 16:10:17 +0300

    feat (administration): create home, board, actu component for administratif part

--- src/app/administration/administration-routing.module.ts
+++ src/app/administration/administration-routing.module.ts
@@ -1,10 +1,27 @@
+import { ActuComponent } from './components/actu/actu.component';
+import { BoardComponent } from './components/board/board.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdministrationComponent } from './administration.component';
 
 const routes: Routes = [
   { path: '', 
-    component: AdministrationComponent
+    component: AdministrationComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'board',
+        component: BoardComponent
+      },
+      {
+        path: 'actu',
+        component: ActuComponent
+      }
+    ]
   }];
 
 @NgModule({
--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AdministrationRoutingModule } from './administration-routing.module';
 import { AdministrationComponent } from './administration.component';
+import { HomeComponent } from './administration/home/home.component';
+import { BoardComponent } from './administration/board/board.component';
+import { ActuComponent } from './administration/actu/actu.component';
 
 
 @NgModule({
   declarations: [
-    AdministrationComponent
+    AdministrationComponent,
+    HomeComponent,
+    BoardComponent,
+    ActuComponent
   ],
   imports: [
     CommonModule,
